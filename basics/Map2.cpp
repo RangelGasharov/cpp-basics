23a7dc2da3a88e1970a77bdf90467b754ce4590c
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-09-06 22:25:05 +0200

    Fixed struct, added example of iteration and at operator

--- basics/Map2.cpp
+++ basics/Map2.cpp
@@ -11,6 +11,15 @@ struct CityRecord
     uint64_t Population;
     double Area;
     double Latitude, Longitude;
+
+    bool operator==(const CityRecord &other) const
+    {
+        return Name == other.Name &&
+               Population == other.Population &&
+               Area == other.Area &&
+               Latitude == other.Latitude &&
+               Longitude == other.Longitude;
+    }
 };
 
 namespace std
@@ -18,12 +27,12 @@ namespace std
     template <>
     struct hash<CityRecord>
     {
-        size_t operator()(const CityRecord &key)
+        size_t operator()(const CityRecord &key) const
         {
             return hash<std::string>()(key.Name);
         }
-    }
-};
+    };
+}
 
 int main()
 {
@@ -34,6 +43,17 @@ int main()
     foundedMap[citiesMap["Berlin"]] = 1200;
     citiesMap["Paris"] = CityRecord{"Paris", 2000000, 100, 40, 10};
     citiesMap["Shanghai"] = CityRecord{"Shanghai", 24000000, 2000, 45, 85};
+
+    const auto &cities = citiesMap;
+    if (cities.find("Berlin") != cities.end())
+    {
+        const CityRecord &berlinData = cities.at("Berlin");
+    }
+
+    for (auto &[name, city] : citiesMap)
+    {
+        std::cout << name << "\n Population:" << city.Population << "\n Area:" << city.Area << std::endl;
+    }
 
     CityRecord &berlinData = citiesMap["Berlin"];
     std::cout << berlinData.Name << std::endl;
